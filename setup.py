a37494719ec5a58aa4ff295f07eda2714804c10f
e-mon <emon18@icloud.com>
2018-10-17 16:52:16 +0900

    fix: dependencies

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.req) for ir in install_reqs]
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
 
 setup(
     name='drctrl',
-    version='0.0.7',
+    version='0.0.9',
     description="Automatically configuration tool for DataRobot.",
     author='e-mon',
     author_email='emon18@icloud.com',
@@ -19,7 +18,7 @@ setup(
         'drctrl' : ['lib/schema/base_schema.yml', 'lib/schema/project_id_is_supplied.yml'],
     },
     entry_points={'console_scripts': ['drctrl = drctrl.cli:main']},
-    install_requires=reqs,
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
